1ab982f589d1d9b8bc196a0bdb73b98f3ed55f9c
osh-een <oisinbell09@gmail.com>
2025-03-08 17:11:49 +0000

    refactored skeleton code and Q4

--- src/main/java/DAOs/MySqlProductDao.java
+++ src/main/java/DAOs/MySqlProductDao.java
@@ -2,7 +2,83 @@ package DAOs;
 
 import DTOs.Product;
 import Exceptions.DaoException;
+import DAOs.MySqlDao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
 public class MySqlProductDao extends MySqlDao implements ProductDaoInterface{
+    @Override
+    public List<Product> getAllProducts() throws DaoException {
+        return null;
+    }
+
+    @Override
+    public Product getProductById(int id) throws DaoException {
+        return null;
+    }
+
+    @Override
+    public void deleteProductById(int id) throws DaoException {
+
+    }
+
+    @Override
+    public void addProduct(Product p) throws DaoException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = this.getConnection();
+
+            String query = "INSERT INTO Products (ProductId, ProductDescription, Size, UnitPrice, SupplierId) VALUES (?, ?, ?, ?, ?)";
+            preparedStatement = connection.prepareStatement(query);
+
+            String ProductId = p.getId();
+            String ProductDescription = p.getDescription();
+            String Size = p.getSize();
+            double unitPrice = p.getPrice();
+            String supplierId = p.getSupplierId();
+
+            preparedStatement.setString(1, ProductId);
+            preparedStatement.setString(2, ProductDescription);
+            preparedStatement.setString(3, Size);
+            preparedStatement.setDouble(4, unitPrice);
+            preparedStatement.setString(5, supplierId);
+
+            int rowsInserted = preparedStatement.executeUpdate();
+
+            if (rowsInserted > 0) {
+                System.out.println("Product added successfully!");
+            } else {
+                System.out.println("Product not added!");
+            }
+        } catch (SQLException e) {
+            throw new DaoException("addProductResultSet() " + e.getMessage());
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    freeConnection(connection);
+                }
+            } catch (SQLException e) {
+                throw new DaoException("addProduct() " + e.getMessage());
+            }
+        }
+    }
+
+    @Override
+    public void updateProduct(int id, Product p) throws DaoException {
+
+    }
+
+//    @Override
+//    public List<Player> findPlayersApplyFilter(playerAgeComparator) throws DaoException {
+//        return null;
+//    }
 
 }
--- src/main/java/DAOs/ProductDaoInterface.java
+++ src/main/java/DAOs/ProductDaoInterface.java
@@ -1,5 +1,42 @@
 package DAOs;
 
-public interface ProductDaoInterface {
+import DTOs.Product;
+import Exceptions.DaoException;
 
+import java.util.List;
+
+public interface ProductDaoInterface {
+//  Feature 1 – Get all Products
+//              e.g. List<Product> getAllProducts() -
+//              return a List of all the Products and display the returned list.
+
+    public List<Product> getAllProducts() throws DaoException;
+
+//  Feature 2 – Find and Display (a single) Product by Key
+//              e.g. Product getProductById(id ) – return a single entity (DTO)
+//              and display its contents.
+
+    public Product getProductById(int id) throws DaoException;
+
+//  Feature 3 – Delete an Entity by key
+//              e.g. deleteProductById(id) – remove specified Product from database
+
+    public void deleteProductById(int id) throws DaoException;
+
+//  Feature 4 – Insert a Product (gather data, instantiate a Product object, pass
+//              into DAO method for insertion in DB)
+//              e.g. Product insertProduct(Product p)
+//              return new Product (Product DTO) that includes the assigned auto-id.
+
+    public void addProduct(Product p) throws DaoException;
+
+//  Feature 5 – Update an existing Product by ID using supplied Product object
+//              e.g. updateProduct(int id, Product p) – executes specified updates
+
+    public void updateProduct(int id, Product p) throws DaoException;
+
+//  Feature 6 – Get list of entities matching a filter (based on DTO object)
+//              e.g. List<Product> findProductApplyFilter( ProductAgeComparator)
+
+    //public List<Product> findPlayersApplyFilter(ProductPriceComparator) throws DaoException;
 }
