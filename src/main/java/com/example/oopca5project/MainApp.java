59c8c48cb12723673a4534712725cf3cff131e3a
osh-een <oisinbell09@gmail.com>
2025-03-09 00:38:26 +0000

    refactored Q4 + Q5. added comments

--- src/main/java/com/example/oopca5project/MainApp.java
+++ src/main/java/com/example/oopca5project/MainApp.java
@@ -13,6 +13,8 @@ public class MainApp {
 
     static Scanner sc = new Scanner(System.in);
 
+    static String id;
+
     public static void main(String[] args) {
         System.out.println("OOP-CA5 PROJECT");
 
@@ -30,33 +32,37 @@ public class MainApp {
                 "End application"
         };
 
-        Methods.menuOptions(options);
+        int choice;
 
-        int choice = Methods.validateRange(1, 7);
+        do{
+            Methods.menuOptions(options);
 
-        switch (choice) {
-            case 1:
-                getAllProducts();
-                break;
-            case 2:
-                System.out.println("Finding product by ID...");
-                break;
-            case 3:
-                System.out.println("Deleting product by ID...");
-                break;
-            case 4:
-                addProduct();
-                break;
-            case 5:
-                updateProduct();
-                break;
-            case 6:
-                System.out.println("Filtering products...");
-                break;
-            case 7:
-                System.out.println("Ending application. Goodbye!");
-                break;
-        }
+            choice = Methods.validateRange(1, 7);
+
+            switch (choice) {
+                case 1:
+                    getAllProducts();
+                    break;
+                case 2:
+                    System.out.println("Finding product by ID...");
+                    break;
+                case 3:
+                    System.out.println("Deleting product by ID...");
+                    break;
+                case 4:
+                    addProduct();
+                    break;
+                case 5:
+                    updateProduct();
+                    break;
+                case 6:
+                    System.out.println("Filtering products...");
+                    break;
+                case 7:
+                    System.out.println("Ending application. Goodbye!");
+                    break;
+            }
+        }while(choice != 7);
     }
 
     // Question 1
@@ -91,28 +97,10 @@ public class MainApp {
     // Question 4
     public static void addProduct() {
         try {
-            String id, product_description, size, supplier_id;
-            double unit_price;
-            Product p;
-
-            // validation of ID not added yet need Q2 done
             System.out.println("Enter product id (e.g. 'product1', 'product2'): ");
             id = sc.nextLine();
 
-            System.out.println("Enter product description: ");
-            product_description = sc.nextLine();
-
-            System.out.println("Enter product size: ");
-            size = sc.nextLine();
-
-            System.out.println("Enter product unit_price: ");
-            unit_price = sc.nextDouble();
-
-            System.out.println("Enter product supplier_id (e.g. 'supplier1', 'supplier2'): ");
-            supplier_id = sc.next();
-
-            p = new Product(id, product_description, size, unit_price, supplier_id);
-            IProductDao.addProduct(p);
+            IProductDao.addProduct(Methods.getProduct(id));
         }
         catch (DaoException e) {
             e.printStackTrace();
@@ -122,27 +110,10 @@ public class MainApp {
     // Question 5
     public static void updateProduct() {
         try {
-            String id, product_description, size, supplier_id;
-            double unit_price;
-            Product p;
-
             System.out.println("Enter product id you wish to update (e.g. 'product1', 'product2'): ");
             id = sc.nextLine();
 
-            System.out.println("Enter product description: ");
-            product_description = sc.nextLine();
-
-            System.out.println("Enter product size: ");
-            size = sc.nextLine();
-
-            System.out.println("Enter product unit_price: ");
-            unit_price = sc.nextDouble();
-
-            System.out.println("Enter product supplier_id (e.g. 'supplier1', 'supplier2'): ");
-            supplier_id = sc.next();
-
-            p = new Product(id,product_description,size,unit_price,supplier_id);
-            IProductDao.updateProduct(id, p);
+            IProductDao.updateProduct(id, Methods.getProduct(id));
         }catch(DaoException e) {
             e.printStackTrace();
         }
--- src/main/java/com/example/oopca5project/Methods.java
+++ src/main/java/com/example/oopca5project/Methods.java
@@ -1,14 +1,18 @@
 package com.example.oopca5project;
 
+import com.example.oopca5project.DTOs.Product;
+
 import java.util.Scanner;
 
 public class Methods {
     static Scanner sc = new Scanner(System.in);
 
     public static void menuOptions(String[] options) {
+        System.out.println();
         for(int i = 1; i <= options.length; i++) {
             System.out.println(i + ". " + options[i-1]);
         }
+        System.out.println();
     }
 
     public static int validateRange(int min, int max) {
@@ -34,7 +38,76 @@ public class Methods {
         return input;
     }
 
-    public static void getProduct() {
+    public static double validateDoubleRange(double min) {
 
+        // initializing variable
+        double input = 0;
+
+        // loop to go on forever until correct input is achieved
+        while(true) {
+
+            // checking if next input is a double
+            if(sc.hasNextDouble()) {
+
+                // when input is a double assign it to a variable
+                input = sc.nextDouble();
+
+                // check if input is within range
+                if(input < min) {
+
+                    // input is wrong -> print out error message
+                    System.out.println("Please enter a valid option (" +min+ "or higher)");
+
+                }
+                else {
+
+                    // input is correct -> break out of loop
+                    break;
+
+                }
+            } else {
+
+                // input was not a double -> print out error message
+                System.out.println("Please enter a valid number");
+
+                // get rid of stray \n (new line) character
+                sc.next();
+            }
+        }
+
+        // input is a double + in range -> input is returned
+        return input;
+
+    }
+
+    public static Product getProduct(String id) {
+
+        // initializing variables
+        String product_description, size, supplier_id;
+        double unit_price;
+
+        // getting rid of stray \n (new line) character
+        sc.nextLine();
+
+        // getting product description
+        System.out.println("Enter product description: ");
+        product_description = sc.nextLine();
+
+        // getting product size
+        System.out.println("Enter product size: ");
+        size = sc.nextLine();
+
+        // getting product unit price
+        System.out.println("Enter product unit price: ");
+        unit_price = validateDoubleRange(0);
+        sc.nextLine();
+
+        // getting product supplier id
+        System.out.println("Enter product supplier id (e.g. 'supplier1', 'supplier2'): ");
+        supplier_id = sc.nextLine();
+
+        // returns newly made product object
+        return new Product(id,product_description,size,unit_price,supplier_id);
+        
     }
 }
