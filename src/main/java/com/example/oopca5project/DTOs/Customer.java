75591855f1907fc60e652ac154b8a40e9dba8405
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-04-02 10:18:58 +0100

    Created Customer class with getters and setters

---
+++ src/main/java/com/example/oopca5project/DTOs/Customer.java
@@ -0,0 +1,33 @@
+package com.example.oopca5project.DTOs;
+
+public class Customer {
+    private int customer_id;
+    private String customer_name;
+    private String customer_email;
+    private String customer_address;
+
+    public Customer() { }
+
+    public Customer(int id, String name, String email, String address) {
+        customer_id = id;
+        customer_name = name;
+        customer_email = email;
+        customer_address = address;
+    }
+
+    public int getId() { return customer_id; }
+
+    public void setId(int id) { customer_id = id; }
+
+    public String getName() { return customer_name; }
+
+    public void setName(String name) { customer_name = name; }
+
+    public String getEmail() { return customer_email; }
+
+    public void setEmail(String email) { customer_email = email; }
+
+    public String getAddress() { return customer_address; }
+
+    public void setAddress(String address) { customer_address = address; }
+}
